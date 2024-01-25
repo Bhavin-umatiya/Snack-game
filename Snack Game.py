c21fcfc6d2c2fc851924f2f9c9950761727372cc
Bhavin1706 <umatiyabhavin@gmail.com>
2024-01-25 21:23:19 +0530

    first commit

---
+++ Snack Game.py
@@ -0,0 +1,32 @@
+import random
+
+def check(comp,user):
+    if comp == user:
+        return 0
+
+    if (comp == 0 and user ==1):
+        return -1
+
+    if (comp == 1 and user ==2):
+        return -1
+
+    if (comp == 2 and user ==0):
+        return -1
+    return 1
+
+comp = random.randint(0,2)
+user = int(input("0 for snak,  1 for water ,  2 for gun"))
+
+score = check(comp,user)
+
+print("you: ", user)
+print("comuter: ",comp)
+
+if(score == 0):
+    print("its win")
+elif (score == -1):
+    print("u lose")
+else:
+    print("u won")
+
+
